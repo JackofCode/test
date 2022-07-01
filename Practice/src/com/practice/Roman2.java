ea3fbb4f2172b0a9d3b39d2cc9bc113beb821613
JackofCode <dhruvp3958@gmail.com>
2022-07-01 15:51:27 -0400

    Changed sysout

--- Practice/src/com/practice/Roman2.java
+++ Practice/src/com/practice/Roman2.java
@@ -21,7 +21,7 @@ public class Roman2 {
 		    hash.put("C",100);
 		    hash.put("D",500);
 		    hash.put("M",1000);
-		    
+		    System.out.println("Github push from Eclipe");
 		    int result=0;
 		    for(int i=0; i<s.length(); i++){
 		        if(i!=s.length()-1 && (hash.get(Character.toString(s.charAt(i))) < hash.get(Character.toString(s.charAt(i+1))))){
@@ -32,6 +32,8 @@ public class Roman2 {
 		            result=result+hash.get(Character.toString(s.charAt(i)));
 		    }
 		    return result;
+		    
+		    
 		
 	}
 }
